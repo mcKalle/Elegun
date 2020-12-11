e0eb97756a582fcc9cca370a61fb8c8a6d5e6458
Philipp Kalenda <philipp.kalenda@lieberlieber.com>
2020-12-11 01:36:58 +0100

    added new sprites for shield ui + new scripts for UI event handling

---
+++ Elegun/Assets/Scripts/Data/InventoryElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Data
+{
+	public class InventoryElement
+	{
+		public InventoryElement(Element element, int count)
+		{
+			Count = count;
+			Element = element;	
+		}
+
+		public int Count { get; set; }
+		public Element Element { get; set; }
+	}
+}
---
+++ Elegun/Assets/Scripts/ElementalShield.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementalShield : MonoBehaviour
+{
+	[HideInInspector]
+	public Element element;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+
+	}
+}
